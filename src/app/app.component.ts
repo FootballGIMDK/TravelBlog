f8c8e20aba56fce60568ff91e80fe23b06c51d7b
Igor Gorobets <>
2021-08-17 18:17:03 +0300

    angular basic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
   title = 'TravelBlog';
+  number = 42;
+
+  inputValue = '';
+
+  onInput(event: KeyboardEvent) {
+    this.inputValue = (<HTMLInputElement>event.target).value;
+  }
+
+  onClick() {
+    console.log('click');
+  }
+
+  now: Date = new Date();
 }
